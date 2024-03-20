4429c4840ae307b3d5bc05561202fd8f26dc8131
TomCroot <thomascroot@hotmail.co.uk>
2024-03-20 14:40:21 +0000

    first commit

---
+++ src/user/user.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {User} from './models/user.entity';
+
+@Module({
+  imports:[
+    TypeOrmModule.forFeature([User])
+  ],
+  controllers: [UserController]
+})
+export class UserModule {
+
+}
